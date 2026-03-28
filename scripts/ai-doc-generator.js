32c678aaf5d9468a20b6d71ed0d72e3ae47c6964
Karan1Nasare <karan@codnestx.com>
2026-03-29 02:57:04 +0530

    final workflow fix

--- scripts/ai-doc-generator.js
+++ scripts/ai-doc-generator.js
@@ -37,7 +37,7 @@ function getContext() {
 // Call OpenAI
 async function generateAI(commitMsg, diff, context) {
   const res = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
     {
       method: "POST",
       headers: {
